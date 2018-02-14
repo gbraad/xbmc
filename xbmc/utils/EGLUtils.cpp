820eb6298d8d63ff44378699856eaa8a7cc5d918
Lukas Rusak <lorusak@gmail.com>
2018-02-13 16:07:26 -0800

    utils/EGLUtils: add class CEGLImage

--- xbmc/utils/EGLUtils.cpp
+++ xbmc/utils/EGLUtils.cpp
@@ -37,3 +37,52 @@ void CEGLUtils::LogError(const std::string& what)
 {
   CLog::Log(LOGERROR, "%s (EGL error %d)", what.c_str(), eglGetError());
 }
+
+/* --- CEGLImage -------------------------------------------*/
+
+CEGLImage::CEGLImage(EGLDisplay display) :
+  m_display(display)
+{
+  eglCreateImageKHR = (PFNEGLCREATEIMAGEKHRPROC)eglGetProcAddress("eglCreateImageKHR");
+  eglDestroyImageKHR = (PFNEGLDESTROYIMAGEKHRPROC)eglGetProcAddress("eglDestroyImageKHR");
+  glEGLImageTargetTexture2DOES = (PFNGLEGLIMAGETARGETTEXTURE2DOESPROC)eglGetProcAddress("glEGLImageTargetTexture2DOES");
+}
+
+bool CEGLImage::CreateImage(eglAttrs imageAttrs)
+{
+  std::vector<EGLint> attr =
+  {
+    EGL_WIDTH, imageAttrs.width,
+    EGL_HEIGHT, imageAttrs.height,
+    EGL_LINUX_DRM_FOURCC_EXT, imageAttrs.fourcc,
+  };
+
+  for (int i = 0; i < imageAttrs.num_planes; i++)
+  {
+    attr.emplace_back(egl_dmabuf_plane_fd_attr[i]);
+    attr.emplace_back(imageAttrs.fd[i]);
+    attr.emplace_back(egl_dmabuf_plane_offset_attr[i]);
+    attr.emplace_back(imageAttrs.offset[i]);
+    attr.emplace_back(egl_dmabuf_plane_pitch_attr[i]);
+    attr.emplace_back(imageAttrs.stride[i]);
+  }
+
+  attr.emplace_back(EGL_NONE);
+
+  m_image = eglCreateImageKHR(m_display, EGL_NO_CONTEXT, EGL_LINUX_DMA_BUF_EXT, nullptr, attr.data());
+
+  if(m_image)
+    return true;
+
+  return false;
+}
+
+void CEGLImage::UploadImage(GLenum textureTarget)
+{
+  glEGLImageTargetTexture2DOES(textureTarget, m_image);
+}
+
+void CEGLImage::DestroyImage()
+{
+  eglDestroyImageKHR(m_display, m_image);
+}
